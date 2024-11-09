35423d178cc366839c3fb48dbd4d5d924e5712d2
Pablo Paez <pablopaez2307@gmail.com>
2024-11-09 17:15:48 -0300

    first commit

---
+++ database/seeders/AdminDataSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
+
+
+
+class AdminDataSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'photo' => 'image.png',
+            'password' => Hash::make('111'),
+        ]);
+    }
+}
